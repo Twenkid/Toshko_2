3a99849eb9451a9a30bc562d69863bfd50699584
Todor Arnaudov <twenkid@gmail.com>
2026-05-31 02:37:55 +0300

    Create copy_toshko2.bat

---
+++ Deploy/copy_toshko2.bat
@@ -0,0 +1,29 @@
+REM Copy Toshko2 files (from developmental folder etc.) copy_toshko2.bat <target_dir>
+REM Check for more files, this is a version from 2013; the EXE FILE is different:
+REM Toshko2076 etc.; Glas2B_WindowsTest (developmental name)// twenkid: 31.5.2026
+copy .\contoursAlpha.txt	%1
+copy .\latincyr.txt	%1
+copy .\glasni.txt	%1
+copy .\slujebni1.txt	%1
+copy .\acc2.cfg	%1
+copy .\acc3.cfg	%1
+copy .\acc4.cfg	%1
+copy .\acc5.cfg	%1
+copy .\acc6.cfg	%1
+copy .\acc_vupros.cfg	%1
+copy .\vuproslist.cfg	%1
+copy .\fonemi0.txt	%1
+copy .\znaci.znc	%1
+copy .\nizove.dll	%1
+copy .\fonemi0.txt	%1
+copy .\osnova_glas.txt	%1
+copy .\osnova_toshko_2.cfg	%1
+copy .\slivane.dll	%1
+copy .\bezzvuk.dll	%1
+copy .\paket.dll	%1
+copy .\wavhdr.wav	%1
+copy .\leleta.dll	%1
+copy .\kusidulgi.dll	%1
+copy .\lame_enc.dll	%1
+copy .\Toshko-2_01-Release-Alpha-201-19-3-2013.exe	%1
+copy .\toshko2-201-intro1.jpg	%1
